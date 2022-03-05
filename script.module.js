e7f8b99c3241d466cea377aee14ae554a4048e79
Giorgio Fausto Canu <gfc9110@gmail.com>
2022-03-05 02:25:35 +0100

    comment cool test

--- script.module.js
+++ script.module.js
@@ -1,9 +1,12 @@
 import * as THREE from "three";
+import { Color } from "three";
 //import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
 
 function main() {
   const canvas = document.querySelector("#c");
   const renderer = new THREE.WebGLRenderer({ canvas });
+
+  //renderer.shadowMap.enabled = true;
 
   const fov = 75;
   const aspect = 2; // the canvas default
@@ -14,6 +17,19 @@ function main() {
   camera.position.z = 2;
 
   const scene = new THREE.Scene();
+  scene.background = new Color(0x222222);
+
+  const light = new THREE.DirectionalLight(0xffffff);
+  //light.castShadow = true;
+
+  //light.shadow.mapSize.x = 1024;
+  //light.shadow.mapSize.y = 1024;
+
+  light.position.x = 1;
+  light.position.z = 1;
+  light.position.y = 0.3;
+
+  scene.add(light);
 
   /*const fbxLoader = new FBXLoader();
   fbxLoader.load("./models/BaseShape3D.fbx", (object) => {
@@ -21,20 +37,40 @@ function main() {
     scene.add(object);
   });*/
 
+  const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({ color: 0xffffff })
+  );
+
+  //plane.receiveShadow = true;
+
+  plane.position.z = -1;
+
+  scene.add(plane);
+
+  const helper = new THREE.CameraHelper(light.shadow.camera);
+
+  //scene.add(helper);
+
   const boxWidth = 0.2024465;
   const boxHeight = 0.53245;
   const boxDepth = 0.2024465;
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+  const material = new THREE.MeshStandardMaterial({ color: 0xff2222 });
+  new THREE.MeshStandardMaterial();
 
   const short = new THREE.Mesh(geometry, material);
+  //short.castShadow = true;
+  //short.receiveShadow = true;
   short.position.y = 0.53245 / 2;
   short.position.x = 0.2024465 / 2;
   const long = new THREE.Mesh(
     new THREE.BoxGeometry(0.86288, 0.2024465, 0.2024465),
     material
   );
+  //long.castShadow = true;
+  //long.receiveShadow = true;
   long.position.x = 0.86288 / 2;
 
   long.position.y = 0.2024465 / 2;
